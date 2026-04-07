90890670b59ebddd4bbd4b0eca17e647cfcf7730
Ahesanali20 <kadiwalaahesanali20@gmail.com>
2026-04-07 15:27:36 +0530

    sir added

--- Menu.py
+++ Menu.py
@@ -13,7 +13,7 @@ print("Burger : Rs.40\nPasta : Rs.50\nCoffee : Rs.60\nSalad : Rs.70\nPizza : Rs.
 
 order_total = 0
 
-order_item = input("Enter the name of item you want to order : ")
+order_item = input("Enter the name of item you want to order : ").capitalize()
 
 if order_item in menu:
     order_total += menu[order_item]
@@ -24,7 +24,8 @@ else:
 
 another_order = input("Do you want order another item ? (yes/no)")
 while another_order.lower() == "yes":
-    order_item_2 = input("Enter the name of another item you want to order : ")
+    order_item_2 = input(
+        "Enter the name of another item you want to order : ").capitalize()
     if order_item_2 in menu:
         order_total += menu[order_item_2]
         print(f"Your item {order_item_2} has been Added to your order")
@@ -35,4 +36,4 @@ while another_order.lower() == "yes":
     another_order = input("Do you want order another item ? (yes/no)")
     if another_order != "yes":
         break
-print(f"Total Amount of your order is {order_total}.")
+print(f"Sir, Total Amount of your order is {order_total}.")
